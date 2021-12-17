c4d6196b2efe2bb2c7eefabc63d4f02831700100
Sebastião Teixeira <steixeira2004@gmail.com>
2021-12-17 23:10:45 +0000

    Implementated transition between pages

--- src/components/SubjectsMenu.vue
+++ src/components/SubjectsMenu.vue
@@ -1,8 +1,11 @@
 <template>
-  <div id="subjects-container">
-    <div v-for="subject in subjects" :key="subject" class="subject">
-        <button>{{ subject }}</button>
-    </div>
+  <div id="subjects-container" :class="{ 'right-slide' : IsRightSlide}">
+    <button v-for="subject in subjects[block]" 
+			:key="subject"
+			class="w3-btn w3-white w3-hover-light-grey w3-round-xlarge"
+			@click="GoToContentPage(subject)">
+        <h2><b>{{ subject }}</b></h2>
+    </button>
 		<p>{{ current_block_str }}</p>
   </div>
 </template>
@@ -11,22 +14,39 @@
 export default {
 	data() {
 		return {
-			subjects: 
-				["Teste1", "Teste2"]
+			subjects: [["Matemática"], ["esate"]],
+			IsRightSlide: false
 		}
 	},
-	props: ['block'],
-	computed: {}
+	emits: ["next-page"],
+	methods: {
+		GoToContentPage(subject) {
+			this.IsRightSlide = true
+			this.$emit('next-page', subject);
+		}
+	},
+	props: ['block']
 }
 </script>
 
 <style scoped>
 	#subjects-container {
 		display: flex;
+		justify-content: center;
 	}
 
 	button {
 		margin : 10px;
 		padding: 12px;
+	}
+
+	.right-slide {
+		position:relative;
+		animation:right-slide 0.4s;
+		animation-fill-mode: forwards;
+	}
+	@keyframes right-slide{
+		from{right:0px;opacity:1}
+		to{right:960px;opacity:0}
 	}
 </style>
